21934365ef4740dd54bae8f2b24ec1d9e58a7c51
Ivan Shyrko <ishyrko@gmail.com>
2026-05-20 23:02:56 +0300

    Seo

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -69,6 +69,53 @@ export default function RootLayout({
           <Toaster />
           <CookieBanner />
         </QueryProvider>
+
+        {/* Google tag (gtag.js) */}
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-M7RYVGGSKK"
+            strategy="afterInteractive"
+        />
+        <Script
+            id="google-analytics-gtag"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-M7RYVGGSKK');
+`,
+            }}
+        />
+        {/* /Google tag (gtag.js) */}
+        {/* Yandex.Metrika counter */}
+        <Script
+            id="yandex-metrika"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109330583', 'ym');
+
+    ym(109330583, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+`,
+            }}
+        />
+        <noscript>
+          <div>
+            <img
+                src="https://mc.yandex.ru/watch/109330583"
+                style={{ position: "absolute", left: "-9999px" }}
+                alt=""
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
       </body>
     </html>
   );
